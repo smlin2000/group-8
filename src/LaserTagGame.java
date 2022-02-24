10c62eeb412cc3ab1a7d058e7ef6c4fa27840326
kk101420 <98425903+kk101420@users.noreply.github.com>
2022-02-23 18:08:51 -0600

    Update LaserTagGame.java
    commit

--- src/LaserTagGame.java
+++ src/LaserTagGame.java
@@ -14,12 +14,13 @@ public class LaserTagGame extends JFrame {
   
   public LaserTagGame() {
   	// Show Splash Screen
-  	setContentPane(new JPanel()); //TODO Make splash screen here
-  	setBounds(50, 50, 600, 410);
+  	JPanel SplashScreen = new JPanel(new BorderLayout());
+	SplashScreen.add(new JLabel(new ImageIcon("logo.jpg")), BorderLayout.CENTER);
+  	setContentPane(SplashScreen);
+  	setBounds(50, 50, 643, 410);
   	setVisible(true);
   	setTitle("Splash Screen");
-
-	  
+  	
   	// Sleep while showing splash screen
   	try {
 			Thread.sleep(2500);
